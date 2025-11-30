8c24a4904290c40a3ca9974ff84493bf51e57fff
Jon-Dickinson <jd.designs.cpt@gmail.com>
2025-11-30 06:43:47 +0200

    fixed critical security issue

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -5,6 +5,38 @@ const nextConfig = {
     styledComponents: true,
   },
   output: 'standalone',
+  async headers() {
+    return [
+      {
+        source: '/_next/static/chunks/:path*.js',
+        headers: [
+          { key: 'X-Content-Type-Options', value: 'nosniff' },
+          { key: 'Content-Type', value: 'application/javascript; charset=utf-8' },
+        ],
+      },
+      {
+        source: '/_next/static/:path*.js',
+        headers: [
+          { key: 'X-Content-Type-Options', value: 'nosniff' },
+          { key: 'Content-Type', value: 'application/javascript; charset=utf-8' },
+        ],
+      },
+      {
+        source: '/_next/static/:path*.css',
+        headers: [
+          { key: 'X-Content-Type-Options', value: 'nosniff' },
+          { key: 'Content-Type', value: 'text/css; charset=utf-8' },
+        ],
+      },
+      {
+        source: '/((?!api/|_next/).*)',
+        headers: [
+          { key: 'X-Content-Type-Options', value: 'nosniff' },
+          { key: 'Content-Type', value: 'text/html; charset=utf-8' },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
